407e9240ceb768ae01ff53fb06ab5519ad30eec2
chenshanru <chshru@qq.com>
2018-10-28 09:57:48 +0800

    ListAty添加列表点击

--- java/com/chshru/music/ui/main/list/ListActivity.java
+++ java/com/chshru/music/ui/main/list/ListActivity.java
@@ -3,11 +3,13 @@ package com.chshru.music.ui.main.list;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.chshru.music.R;
 import com.chshru.music.base.ActivityBase;
 import com.chshru.music.base.MusicApp;
 import com.chshru.music.ui.main.search.SearchResultAdapter;
+import com.chshru.music.ui.main.search.SearchResultAdapter.OnItemClickListener;
 import com.chshru.music.util.Song;
 
 import java.util.List;
@@ -16,6 +18,7 @@ public class ListActivity extends ActivityBase {
 
     private RecyclerView mRecycler;
     private SearchResultAdapter mAdapter;
+    private MusicApp mApp;
 
     @Override
     protected int getLayoutId() {
@@ -31,14 +34,22 @@ public class ListActivity extends ActivityBase {
 
     @Override
     protected void initialize() {
-        MusicApp app = (MusicApp) getApplication();
-        String title = app.getListData().getTitle();
-        int pos = app.getListData().getPos();
-        List<Song> list = app.getListData().getList(pos);
+        mApp = (MusicApp) getApplication();
+        String title = mApp.getListData().getTitle();
+        int pos = mApp.getListData().getPos();
+        List<Song> list = mApp.getListData().getList(pos);
         mAdapter = new SearchResultAdapter(list, getMainLooper());
         mRecycler = findViewById(R.id.list_recycler);
         mRecycler.setLayoutManager(new LinearLayoutManager(this));
         mRecycler.setAdapter(mAdapter);
         findViewById(R.id.back).setOnClickListener(view -> finish());
+        mAdapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View v) {
+                int pos = mRecycler.getChildAdapterPosition(v);
+                Song song = mAdapter.get(pos);
+                mApp.getPlayer().prepare(song);
+            }
+        });
     }
 }
